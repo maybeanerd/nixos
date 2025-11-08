e3629aea41a75424199dc58c930c136285d9317e
maybeanerd <sebastian@diluz.io>
2025-11-08 23:44:11 +0100

    feat: try using pulseaudio instead of pipewire

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -118,21 +118,14 @@ in
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
-  # Enable sound with pipewire.
-  services.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
+  # Enable sound with pulseaudio.
+  services.pipewire.enable = lib.mkForce false; 
+  services.pulseaudio = {
     enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
+    support32Bit = true;
+    package = pkgs.pulseaudioFull;
   };
+  nixpkgs.config.pulseaudio = true;
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
@@ -144,7 +137,7 @@ in
   users.users.basti = {
     isNormalUser = true;
     description = "Sebastian Di Luzio";
-    extraGroups = [ "networkmanager" "wheel" ];
+    extraGroups = [ "networkmanager" "wheel" "audio" ];
     shell = pkgs.zsh;
     ignoreShellProgramCheck = true; # We install it on home-manager level
 
