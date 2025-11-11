53334dd3d37f560733f36138fe1a5f8dde9ec03e
Sebastian Di Luzio <sebastian@diluz.io>
2025-11-11 17:04:05 +0100

    load home manage in a flake way

--- darwin-work/flake.nix
+++ darwin-work/flake.nix
@@ -5,9 +5,11 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:nix-darwin/nix-darwin/master";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager/release-25.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager }:
   let
     configuration = { pkgs, ... }: {
 
@@ -41,7 +43,10 @@
     # Build darwin flake using:
     # $ darwin-rebuild build --flake .#IUGMQ7JVJV62M2
     darwinConfigurations."IUGMQ7JVJV62M2" = nix-darwin.lib.darwinSystem {
-      modules = [ configuration ];
+      modules = [ 
+        configuration
+        home-manager.darwinModules.home-manager
+      ];
     };
   };
 }
