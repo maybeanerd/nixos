0d63ee0e325379ce3af2d78f2de2b259eb629da1
Sebastian Di Luzio <sebastian@diluz.io>
2025-11-11 16:05:57 +0100

    add darwin

--- darwin-work/flake.nix
+++ darwin-work/flake.nix
@@ -1,66 +1,43 @@
 {
-  description = "Darwin work system flake";
+  description = "Example nix-darwin system flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-25.05-darwin";
-    nix-darwin.url = "github:nix-darwin/nix-darwin/nix-darwin-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:nix-darwin/nix-darwin/master";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-    home-manager.url = "github:nix-community/home-manager/release-25.05";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager }:
+  outputs = inputs@{ self, nix-darwin, nixpkgs }:
   let
-    mkDarwinSystem = username: nix-darwin.lib.darwinSystem {
-      modules = [
-        home-manager.darwinModules.home-manager
-        ({ pkgs, config, ... }: {
-          imports = [
-            (import ./configs/home-manager.nix { inherit username; })
-          ];
+    configuration = { pkgs, ... }: {
+      # List packages installed in system profile. To search by name, run:
+      # $ nix-env -qaP | grep wget
+      environment.systemPackages =
+        [ pkgs.vim
+        ];
 
-          # List packages installed in system profile. To search by name, run:
-          # $ nix-env -qaP | grep wget
-          environment.systemPackages = [ ];
+      # Necessary for using flakes on this system.
+      nix.settings.experimental-features = "nix-command flakes";
 
-          # Allow unfree packages
-          nixpkgs.config.allowUnfree = true;
+      # Enable alternative shell support in nix-darwin.
+      # programs.fish.enable = true;
 
-          # Necessary for using flakes on this system.
-          nix.settings.experimental-features = "nix-command flakes";
+      # Set Git commit hash for darwin-version.
+      system.configurationRevision = self.rev or self.dirtyRev or null;
 
-          # Use the provided user
-          users.users.${username} = {
-            name = username;
-            home = "/Users/${username}";
-          };
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 6;
 
-          # Enable alternative shell support in nix-darwin.
-          # programs.fish.enable = true;
-
-          # Set Git commit hash for darwin-version.
-          system.configurationRevision = self.rev or self.dirtyRev or null;
-
-          # Used for backwards compatibility, please read the changelog before changing.
-          # $ darwin-rebuild changelog
-          system.stateVersion = 6;
-
-          # The platform the configuration will be used on.
-          nixpkgs.hostPlatform = "aarch64-darwin";
-        })
-      ];
+      # The platform the configuration will be used on.
+      nixpkgs.hostPlatform = "aarch64-darwin";
     };
   in
   {
     # Build darwin flake using:
-    # $ darwin-rebuild switch --flake .#<hostname>
-    # You need to create an entry for each machine's hostname
-    darwinConfigurations = {
-      # Your current machine
-      "IUGMQ7JVJV62M2" = mkDarwinSystem "sebastian.di-luzio";
-      
-      # Add more machines here as needed:
-      # "other-hostname" = mkDarwinSystem "other-username";
+    # $ darwin-rebuild build --flake .#IUGMQ7JVJV62M2
+    darwinConfigurations."IUGMQ7JVJV62M2" = nix-darwin.lib.darwinSystem {
+      modules = [ configuration ];
     };
   };
 }
