b2eb898beedcd0fb75a91a7ab7f430d7f14effe5
Sebastian Di Luzio <sebastian@diluz.io>
2025-11-11 17:27:44 +0100

    Update flake.nix

--- darwin-work/flake.nix
+++ darwin-work/flake.nix
@@ -22,6 +22,9 @@
       environment.systemPackages =
         [ ];
 
+      # Allow unfree packages
+      nixpkgs.config.allowUnfree = true;
+
       # Necessary for using flakes on this system.
       nix.settings.experimental-features = "nix-command flakes";
 
