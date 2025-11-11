746cc004186a9b36ca707f3bf35f4cc4a75990de
Sebastian Di Luzio <sebastian@diluz.io>
2025-11-11 16:08:08 +0100

    add home manager

---
+++ darwin-work/configs/home-manager.nix
@@ -0,0 +1,56 @@
+{ config, pkgs, ... }:
+
+let
+  home-manager = builtins.fetchTarball https://github.com/nix-community/home-manager/archive/release-25.05.tar.gz;
+in
+{
+  imports =
+    [
+      (import "${home-manager}/nixos")
+    ];
+
+  # Make Home Manager use the system nixpkgs configuration so it respects
+  # the `nixpkgs.config` settings (e.g. allowUnfree).
+  home-manager.useGlobalPkgs = true;
+
+  home-manager.users.basti = { pkgs, ... }: {
+    programs.zsh = {
+      enable = true;
+      enableCompletion = true;
+      autosuggestion.enable = true;
+      syntaxHighlighting.enable = true;
+      oh-my-zsh = {
+        enable = true;
+        plugins = [
+          "git"
+          "git-auto-fetch"
+        ];
+        theme = "jonathan";
+      };
+      shellAliases = {
+        ll = "ls -la";
+        rb = "sudo nixos-rebuild switch";
+      };
+    };
+
+    home.packages = with pkgs; [
+      # general apps
+      # bitwarden-desktop
+
+      # communication
+
+      # media
+      tidal-hifi
+
+      # software development
+      vscode
+      git
+      github-desktop
+      nodejs_24
+    ];
+
+    # The state version is required and should stay at the version you
+    # originally installed.
+    home.stateVersion = "25.05";
+  };
+}
