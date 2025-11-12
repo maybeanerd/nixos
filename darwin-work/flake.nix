6195c6f4cd0039bed47c7571f516723384bca9a6
Sebastian Di Luzio <sebastian@diluz.io>
2025-11-12 12:02:29 +0100

    feat: add darwin nix (#1)

---
+++ darwin-work/flake.nix
@@ -0,0 +1,66 @@
+{
+  description = "Darwin work system flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-25.05-darwin";
+    nix-darwin.url = "github:nix-darwin/nix-darwin/nix-darwin-25.05";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager/release-25.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager }:
+  let
+    mkDarwinSystem = username: nix-darwin.lib.darwinSystem {
+      modules = [
+        home-manager.darwinModules.home-manager
+        ({ pkgs, config, ... }: {
+          imports = [
+            (import ./configs/home-manager.nix { inherit username; })
+          ];
+
+          # List packages installed in system profile. To search by name, run:
+          # $ nix-env -qaP | grep wget
+          environment.systemPackages = [ ];
+
+          # Allow unfree packages
+          nixpkgs.config.allowUnfree = true;
+
+          # Necessary for using flakes on this system.
+          nix.settings.experimental-features = "nix-command flakes";
+
+          # Use the provided user
+          users.users.${username} = {
+            name = username;
+            home = "/Users/${username}";
+          };
+
+          # Enable alternative shell support in nix-darwin.
+          # programs.fish.enable = true;
+
+          # Set Git commit hash for darwin-version.
+          system.configurationRevision = self.rev or self.dirtyRev or null;
+
+          # Used for backwards compatibility, please read the changelog before changing.
+          # $ darwin-rebuild changelog
+          system.stateVersion = 6;
+
+          # The platform the configuration will be used on.
+          nixpkgs.hostPlatform = "aarch64-darwin";
+        })
+      ];
+    };
+  in
+  {
+    # Build darwin flake using:
+    # $ darwin-rebuild switch --flake .#<hostname>
+    # You need to create an entry for each machine's hostname
+    darwinConfigurations = {
+      # Your current machine
+      "IUGMQ7JVJV62M2" = mkDarwinSystem "sebastian.di-luzio";
+      
+      # Add more machines here as needed:
+      # "other-hostname" = mkDarwinSystem "other-username";
+    };
+  };
+}
