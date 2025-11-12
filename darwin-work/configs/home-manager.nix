c92d5379b648f5c6c249d1d894ffd21dec36458c
Sebastian Di Luzio <sebastian@diluz.io>
2025-11-12 11:24:13 +0100

    feat: set up nvm thefuck and pnpm

--- darwin-work/configs/home-manager.nix
+++ darwin-work/configs/home-manager.nix
@@ -17,13 +17,24 @@
         plugins = [
           "git"
           "git-auto-fetch"
+          "nvm"
         ];
         theme = "jonathan";
       };
       shellAliases = {
         ll = "ls -la";
-        rb = "sudo nixos-rebuild switch";
+        rb = "sudo darwin-rebuild switch";
       };
+      initContent = ''
+        # nvm configuration (external installation)
+        # The oh-my-zsh nvm plugin handles loading nvm and provides zsh completions
+        export NVM_DIR="$HOME/.nvm"
+      '';
+    };
+
+    programs.thefuck = {
+      enable = true;
+      enableZshIntegration = true;
     };
 
     home.packages = with pkgs; [
@@ -40,6 +51,7 @@
       git
       # github-desktop
       nodejs_24
+      nodePackages.pnpm
     ];
 
     # The state version is required and should stay at the version you
