df9b23e435d59817b0a3b56522d239dc31028d55
Sebastian Di Luzio <sebastian@diluz.io>
2026-01-04 14:28:42 +0100

    use open source nvidia drivers

--- nixos/default.nix
+++ nixos/default.nix
@@ -28,7 +28,7 @@
     modesetting.enable = true;
     powerManagement.enable = false;
     powerManagement.finegrained = false;
-    open = false;
+    open = true;
     nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
