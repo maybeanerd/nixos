070e04a2c45cf26b5401bcb083e694cb8268f80b
Sebastian Di Luzio <sebastian@diluz.io>
2026-02-20 20:54:10 +0100

    Update sops.nix

--- nixos/sops.nix
+++ nixos/sops.nix
@@ -7,10 +7,13 @@ let
   user = config.users.users.${username};
 in
 {
+  # Allow the system to use its own SSH key for decryption
+  sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+  # In theory, allow yubikey support for sops decrypt during build, but it doesn't work
+  sops.age.keyFile = "/var/lib/sops-nix/yubikey-identities.txt";
   systemd.tmpfiles.rules = [
     "f /var/lib/sops-nix/yubikey-identities.txt 0640 root users -"
   ];
-  sops.age.keyFile = "/var/lib/sops-nix/yubikey-identities.txt";
 
   sops.defaultSopsFile = ../secrets/nixos.yaml;
 
