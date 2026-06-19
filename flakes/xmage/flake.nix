d0258c60a1138e4f06b5a64ebfda78127201a2ef
Sebastian Di Luzio <sebastian@diluz.io>
2026-06-19 19:41:00 +0200

    feat(xmage): update client version to latest

--- flakes/xmage/flake.nix
+++ flakes/xmage/flake.nix
@@ -18,8 +18,8 @@
         pkgs = nixpkgs.legacyPackages.${system};
 
         # XMage version and source
-        version = "1.4.58-dev_2025-10-06_20-40"; # Update this to change the version
-        sha256 = "sha256-UOtxV+ykDIH+PLjLrC66Rut92IIw2iDHWwvJ2ytmUAs="; # Update this hash when updating the version
+        version = "1.4.59-dev_2026-05-26_22-35"; # Update this to change the version
+        sha256 = "sha256-1nfqxqQOrk9AGYgzuTSUNLUkcwce4N9vs+u7fqWLscA="; # Update this hash when updating the version
         src = pkgs.fetchurl {
           url = "https://xmage.today/files/mage-full_${version}.zip";
           inherit sha256;
