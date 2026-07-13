06cb1efb627d42971c110f4b609a2cbccaa04be1
Sebastian Di Luzio <sebastian.diluzio@liqid.de>
2026-07-13 13:03:49 +0200

    feat: add devenv (#162)
    * feat: add devenv
    
    * add trusted user

--- home-manager/development/shared.nix
+++ home-manager/development/shared.nix
@@ -18,6 +18,7 @@ let
     tldr
     sops
     clamav # antivirus
+    devenv
   ];
 
   nixosPackages =
@@ -131,7 +132,7 @@ in
 
         fu = "nix flake update";
         fl = "nix flake lock";
-        dev = "nix develop";
+        develop = "nix develop";
 
         ff = "fastfetch";
         neofetch = "fastfetch";
